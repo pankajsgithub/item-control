28ec69bed8e52b0433b10d439405534be91354a7
pankajsgithub <pankaj.summerproject2016@gmail.com>
2021-04-12 00:18:44 +0530

    Check

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>This React App is Deployed</h3>
+        <h3>React App Deployed</h3>
       </header>
     </div>
   );
