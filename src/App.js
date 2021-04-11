4635998b23ca9b6bcf2f634a354e0913609ebd47
pankajsgithub <pankaj.summerproject2016@gmail.com>
2021-04-12 00:25:34 +0530

    Modified Text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>React App Deployed</h3>
+        <h3>React App Deployed Online</h3>
       </header>
     </div>
   );
